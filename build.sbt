90d70bb0654bf9feee34e5bc1b5011b692c4c05d
Nikolay Ryabykh <nryabykh@isgneuro.com>
2020-12-14 19:11:37 +0300

    init commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "aoc2020"
+
+version := "0.1"
+
+scalaVersion := "2.13.4"
+
+idePackagePrefix := Some("com.github.nryabykh")
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % Test
+
---
+++ src/main/scala/aoc2020/DayOneReportRepair.scala
@@ -0,0 +1,23 @@
+package com.github.nryabykh.aoc2020
+
+case class DayOneReportRepair(filename: Option[String] = None) {
+
+  def getTwoMultiplication(input: List[Int]): Int = {
+    val second = input.map(x => 2020 - x)
+    input.intersect(second) match {
+      case h :: t :: _ => h * t
+    }
+  }
+
+  def getThreeMultiplication(input: List[Int]): Int = {
+    val calc = input.map(x => {
+      val filtered = input.filter(_ != x)
+      val subtracted = filtered.map(y => 2020 - x - y)
+      filtered.intersect(subtracted) match {
+        case h :: t :: _ => x * h * t
+        case _ => -1
+      }
+    })
+    calc.find(_ != -1).getOrElse(-1)
+  }
+}
